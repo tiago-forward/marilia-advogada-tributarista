079a352ea2633f0141114b07dc94dc0caef867e5
tiagoforward <devtiagoforward@gmail.com>
2024-05-29 12:57:03 -0300

    cleaning a house

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -2,11 +2,8 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main>
       <Hero />
-      <div className="max-w-screen-xl mt-20">
-        <img src="https://educacao.ibpt.com.br/wp-content/uploads/2018/12/principios-do-direito-tributarios.png" alt="" />
-      </div>
     </main>
   );
 }
