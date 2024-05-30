876457a09ab082900b6cea56fe69ff80e8c5dbc5
tiagoforward <devtiagoforward@gmail.com>
2024-05-30 08:51:25 -0300

    finishing settings ComponentAbout

--- src/app/(root)/(home)/about/page.tsx
+++ src/app/(root)/(home)/about/page.tsx
@@ -1,4 +1,4 @@
-export default function About() {
+export default function AboutInfo() {
     return (
         <div>
             <h1 className="">Sobre</h1>
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -9,6 +9,9 @@ const layout = ({ children }: { children: React.ReactNode }) => {
             <Header />
             {children}
             <Footer />
+            <a href="https://wa.me/5583998310461" target='_blank' className='group z-50 fixed right-8 bottom-8 grid place-items-center rounded-full cursor-pointer duration-300 ease-linear p-3 lg:p-4 border-2 border-transparent bg-green-500 hover:bg-green-600 hover:border-green-700'>
+                <img src="https://img.icons8.com/?size=100&id=62855&format=png&color=ffffff" alt="Icone do Whatsapp" className='w-6 lg:w-8' />
+            </a>
         </>
     )
 }
