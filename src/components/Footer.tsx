f239aeb4fe5114d6043f4c0b391439c664271860
tiagoforward <devtiagoforward@gmail.com>
2024-06-01 14:18:23 -0300

    Refactoring and styling the footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,24 +5,28 @@ import { socialLinks } from "@/constants"
 export const Footer = () => {
     return (
         <footer className="w-full flex flex-col items-center justify-center transition-colors duration-500 bg-black text-white gap-4 border-t border-zinc-900 py-10">
-            <div className="flex flex-col items-center">
-                <p className="text-sm text-center">Todos os Direitos Reservados © 2024</p>
-                <span className="text-sm text-amber-600 font-medium">Marília de Souza</span>
-            </div>
-            <nav className="flex">
-                <ul className="flex items-center justify-center gap-2">
-                    {socialLinks.map((link) => (
-                        <li key={link.id} className="duration-200 hover:text-amber-600">
-                            <Link href={link.url} target="_blank">
-                                {link.icon && <link.icon />}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-            <a href="https://advocaciajf.com/" target="_blank"><img src="https://advocaciajf.com/wp-content/uploads/2024/03/Logotipo-Redesenhada-3.png" alt="" className="w-28" /></a>
-            <div className="">
-                <p className="text-xs text-center">Desenvolvido por <span className="text-blue-700 font-bold"><a href="https://www.linkedin.com/in/tiago-lacerda-devfrontend/" target="_blank" className="hover:underline font-cinzel">TiagoForward</a></span></p>
+            <div className="w-full max-w-screen-xl m-auto flex flex-col items-center gap-4">
+                <div className="flex flex-col lg:flex-row items-center lg:justify-between gap-2 w-full">
+                    <span className="text-sm text-amber-600 font-medium uppercase">Marília de Souza</span>
+                    <nav className="flex">
+                        <ul className="flex items-center justify-center gap-2">
+                            {socialLinks.map((link) => (
+                                <li key={link.id} className="duration-200 hover:text-amber-600">
+                                    <Link href={link.url} target="_blank">
+                                        {link.icon && <link.icon />}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+                <div className="w-full flex justify-center lg:justify-start">
+                    <a href="https://advocaciajf.com/" target="_blank"><img src="https://advocaciajf.com/wp-content/uploads/2024/03/Logotipo-Redesenhada-3.png" alt="Logo da Jonathan Ferrari Advocacia Inteligente" className="w-28" /></a>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <p className="text-xs text-center">Desenvolvido por <span className="text-blue-700 font-bold"><a href="https://www.linkedin.com/in/tiago-lacerda-devfrontend/" target="_blank" className="hover:underline font-cinzel">TiagoForward</a></span></p>
+                    <p className="text-sm text-center">Todos os Direitos Reservados Copyright © {new Date().getFullYear()}</p>
+                </div>
             </div>
         </footer>
     )
