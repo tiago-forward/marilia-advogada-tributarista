c82ef58e5fe3c6b5de6fdc74734dbf7a4cc0ad1d
tiagoforward <devtiagoforward@gmail.com>
2024-06-03 21:31:17 -0300

    new finished and responsive services section

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,34 +1,19 @@
 import { ServiceCard } from "./ServiceCard"
 
+import { services } from "@/constants"
+
 export const Service = () => {
     return (
         <section className="w-full flex flex-col items-center justify-center backgroundGrandientService">
             <div className="max-w-screen-xl m-auto py-10">
                 <h2 className='text-center pt-6 lg:pb-6 text-2xl lg:text-4xl font-oswald tracking-wide'>Serviços de Atuação do Direito Tributário</h2>
                 <p className="text-justify tracking-tight font-oswald text-sm lg:text-xl leading-7 lg:leading-9 pt-6">Ofereço uma ampla gama de serviços voltados para questões tributárias, garantindo que nossos clientes estejam em conformidade com todas as leis fiscais aplicáveis</p>
-                <div className="py-6 flex flex-col gap-2">
-                    <ServiceCard
-                        title={"Consultoria Tributária"}
-                        description={"Oferecemos consultoria personalizada para ajudar empresas e indivíduos a entenderem e cumpram suas obrigações fiscais, otimizando impostos pagos e evitando penalidades."}
-                    />
-                    <ServiceCard
-                        title={"Planejamento Fiscal"}
-                        description={"Desenvolvemos estratégias de planejamento fiscal para minimizar a carga tributária, incluindo a reestruturação de negócios, investimentos e transações internacionais."}
-                    />
-                    <ServiceCard
-                        title={"Litígios Tributários"}
-                        description={"Representamos nossos clientes em disputas com as autoridades fiscais, buscando a melhor solução possível dentro do sistema jurídico brasileiro."}
-                    />
-                    <ServiceCard
-                        title={"Auditoria Interna Tributária"}
-                        description={"Realizamos auditorias internas para identificar possíveis irregularidades fiscais, garantindo a conformidade contínua com as regulamentações tributárias."}
-                    />
-                    <ServiceCard
-                        title={"Consultoria Tributária"}
-                        description={"Oferecemos consultoria personalizada para ajudar empresas e indivíduos a entenderem e cumpram suas obrigações fiscais, otimizando impostos pagos e evitando penalidades."}
-                    />
+                <div className="pt-6 gap-6 grid grid-cols-[minmax(300px,_1fr)] sm:grid-cols-2 lg:grid-cols-3">
+                    {services.map((service, index) => (
+                        <ServiceCard key={index} title={service.name} description={service.description} background={service.icon} />
+                    ))}
                 </div>
             </div>
-        </section >
+        </section>
     )
 }
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,13 +1,24 @@
+import Image, { StaticImageData } from "next/image"
+
 interface ServiceCardProps {
     title: string
     description: string
+    background?: StaticImageData
 }
 
-export const ServiceCard = ({title, description}: ServiceCardProps) => {
+export const ServiceCard = ({ title, description, background }: ServiceCardProps) => {
     return (
-        <div className="py-4 px-4 bg-[#f6c69f] rounded-xl shadow-md shadow-amber-600">
-            <h3 className="text-center tracking-wide font-oswald text-base lg:text-xl leading-7 lg:leading-9 pb-4 text-red-800 font-bold">{title}</h3>
-            <p className="text-center font-be-vietnam-pro text-base lg:text-xl lg:leading-9 text-black font-semibold">{description}</p>
-        </div>
+        <>
+            <div className="bg-transparent rounded-xl shadow-[rgba(0,_0,_0,_0.50)_0px_8px_20px] relative">
+                <div className="opacity-90 brightness-50">
+                    {background && (
+                        <Image src={background} alt="Background Image" className="rounded-xl shadow-md " objectFit="cover" />
+                    )}
+                </div>
+                {/* <div className="absolute top-6 right-0 left-0 text-center">_________</div> */}
+                <h3 className="absolute top-4 left-0 right-0 text-center text-lg leading-7 mb-4 mx-4 text-red-600 font-semibold border-b">{title}</h3>
+                <p className="absolute top-14 left-0 right-0 w-full px-4 text-center font-be-vietnam-pro text-base leading-5">{description}</p>
+            </div>
+        </>
     )
 }
