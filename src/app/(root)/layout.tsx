db5f5941fbbdd99a845e09228e0a6522c0c6d73b
tiagoforward <devtiagoforward@gmail.com>
2024-06-23 14:38:11 -0300

    some adjustments

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Image from 'next/image'
+
+import WhatsApp from './../../../public/whatsapp.png'
 
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
@@ -10,7 +13,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
             {children}
             <Footer />
             <a href="https://wa.me/5583998310461" target='_blank' className='group z-50 fixed right-8 bottom-8 grid place-items-center rounded-full cursor-pointer duration-300 ease-linear p-3 lg:p-4 border-2 border-transparent bg-green-500 hover:bg-green-600 hover:border-green-700'>
-                <img src="https://img.icons8.com/?size=100&id=62855&format=png&color=ffffff" alt="Icone do Whatsapp" className='w-6 lg:w-8' />
+                <Image src={WhatsApp} alt="Icone do Whatsapp" width={26} height={26} />
             </a>
         </>
     )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { socialLinks } from "@/constants"
 
@@ -21,7 +22,7 @@ export const Footer = () => {
                     </nav>
                 </div>
                 <div className="w-full flex justify-center lg:justify-start">
-                    <a href="https://advocaciajf.com/" target="_blank"><img src="https://advocaciajf.com/wp-content/uploads/2024/03/Logotipo-Redesenhada-3.png" alt="Logo da Jonathan Ferrari Advocacia Inteligente" className="w-28" /></a>
+                    <a href="https://advocaciajf.com/" target="_blank"><Image src="https://advocaciajf.com/wp-content/uploads/2024/03/Logotipo-Redesenhada-3.png" width={112} height={50} alt="Logo da Jonathan Ferrari Advocacia Inteligente" /></a>
                 </div>
                 <div className="flex flex-col gap-2">
                     <p className="text-xs text-center">Desenvolvido por <span className="text-blue-700 font-bold"><a href="https://www.linkedin.com/in/tiago-lacerda-devfrontend/" target="_blank" className="hover:underline font-cinzel">TiagoForward</a></span></p>
